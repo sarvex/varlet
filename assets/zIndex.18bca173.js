917754d9876871706a16a8d148441314a20cd13d
BeADre <beadre_wang@163.com>
2022-06-21 01:22:13 +0000

    Deploying to gh-page from @ varletjs/varlet@9f3eae77922b11364bccc2b51ce34763619a5631 🚀

---
+++ assets/zIndex.18bca173.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index.283fa2a3.js";import{a as o,w as x}from"./elevation.712e0ad4.js";function m(n,r){const t=o(e.zIndex);return x(n,a=>{a&&(e.zIndex+=r,t.value=e.zIndex)},{immediate:!0}),{zIndex:t}}export{m as u};
