b86eecad7e0dac96cba37b3b3ae43433748e0ec4
haoziqaq <357229046@qq.com>
2022-09-30 08:51:59 +0000

    Deploying to gh-page from @ varletjs/varlet@4920d191798c28f5d850e46f30e1267cbeba2fdd 🚀

--- assets/en-US.b1873f07.js
+++ assets/en-US.b1873f07.js
@@ -1,11 +1,10 @@
-import{_ as t,p as o,f as c,h as r,M as s,q as e,S as n,P as a}from"./vue-router.esm-bundler.e71c168d.js";const h={components:{}},p={class:"varlet-site-doc"},i=s("h1",null,"Dark Mode",-1),d=s("div",{class:"card"},[s("h3",null,"Intro"),s("p",null,"The first party of the component library provides the theme of dark mode. The advantage of dark mode is that it has higher readability in low light environments.")],-1),m={class:"card"},j=s("h3",null,"Toggle Theme",-1),u=s("pre",{class:"hljs"},[s("code",null,[s("span",{class:"hljs-tag"},[a("<"),s("span",{class:"hljs-name"},"script"),a(),s("span",{class:"hljs-attr"},"setup"),a(">")]),s("span",{class:"javascript"},[a(`
-`),s("span",{class:"hljs-keyword"},"import"),a(" dark "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui/es/themes/dark'"),a(`
-`),s("span",{class:"hljs-keyword"},"import"),a(" { StyleProvider } "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui'"),a(`
+import{_ as t,p as o,f as c,h as r,M as s,q as l,S as n,P as a}from"./vue-router.esm-bundler.e71c168d.js";const h={components:{}},p={class:"varlet-site-doc"},i=s("h1",null,"Dark Mode",-1),d=s("div",{class:"card"},[s("h3",null,"Intro"),s("p",null,"The first party of the component library provides the theme of dark mode. The advantage of dark mode is that it has higher readability in low light environments.")],-1),m={class:"card"},u=s("h3",null,"Toggle Theme",-1),j=s("pre",{class:"hljs"},[s("code",null,[s("span",{class:"hljs-tag"},[a("<"),s("span",{class:"hljs-name"},"script"),a(),s("span",{class:"hljs-attr"},"setup"),a(">")]),s("span",{class:"javascript"},[a(`
+`),s("span",{class:"hljs-keyword"},"import"),a(" { StyleProvider, Themes } "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui'"),a(`
 
 `),s("span",{class:"hljs-keyword"},"let"),a(" currentTheme = "),s("span",{class:"hljs-literal"},"null"),a(`
 
 `),s("span",{class:"hljs-keyword"},"const"),a(" toggleTheme = "),s("span",{class:"hljs-function"},"() =>"),a(` {
-  currentTheme = currentTheme ? `),s("span",{class:"hljs-literal"},"null"),a(` : dark
+  currentTheme = currentTheme ? `),s("span",{class:"hljs-literal"},"null"),a(` : Themes.dark
   StyleProvider(currentTheme)
 }
 `)]),s("span",{class:"hljs-tag"},[a("</"),s("span",{class:"hljs-name"},"script"),a(">")]),a(`
@@ -18,4 +17,4 @@ import{_ as t,p as o,f as c,h as r,M as s,q as e,S as n,P as a}from"./vue-router
   `),s("span",{class:"hljs-attribute"},"color"),a(": "),s("span",{class:"hljs-built_in"},"var"),a(`(--color-text);
   `),s("span",{class:"hljs-attribute"},"background-color"),a(": "),s("span",{class:"hljs-built_in"},"var"),a(`(--color-body);
 }
-`)])],-1);function v(b,k,y,f,T,x){const l=o("var-site-code-example");return c(),r("div",p,[i,d,s("div",m,[j,e(l,{"playground-ignore":""},{default:n(()=>[u]),_:1}),_,e(l,{"playground-ignore":""},{default:n(()=>[g]),_:1})])])}const S=t(h,[["render",v]]);export{S as default};
+`)])],-1);function b(v,f,k,y,T,x){const e=o("var-site-code-example");return c(),r("div",p,[i,d,s("div",m,[u,l(e,{"playground-ignore":""},{default:n(()=>[j]),_:1}),_,l(e,{"playground-ignore":""},{default:n(()=>[g]),_:1})])])}const S=t(h,[["render",b]]);export{S as default};
--- assets/zh-CN.3b4f94c5.js
+++ assets/zh-CN.3b4f94c5.js
@@ -1,11 +1,10 @@
-import{_ as t,p as c,f as o,h as r,M as s,q as n,S as e,P as a}from"./vue-router.esm-bundler.e71c168d.js";const p={components:{}},h={class:"varlet-site-doc"},d=s("h1",null,"\u6697\u9ED1\u6A21\u5F0F",-1),i=s("div",{class:"card"},[s("h3",null,"\u4ECB\u7ECD"),s("p",null,"\u7EC4\u4EF6\u5E93\u7B2C\u4E00\u65B9\u63D0\u4F9B\u4E86\u6697\u9ED1\u6A21\u5F0F\u7684\u4E3B\u9898\uFF0C\u6697\u9ED1\u6A21\u5F0F\u7684\u4F18\u52BF\u5728\u4E8E\u5728\u5F31\u5149\u73AF\u5883\u4E0B\u5177\u6709\u66F4\u9AD8\u7684\u53EF\u8BFB\u6027\u3002")],-1),j={class:"card"},u=s("h3",null,"\u5207\u6362\u4E3B\u9898",-1),_=s("pre",{class:"hljs"},[s("code",null,[s("span",{class:"hljs-tag"},[a("<"),s("span",{class:"hljs-name"},"script"),a(),s("span",{class:"hljs-attr"},"setup"),a(">")]),s("span",{class:"javascript"},[a(`
-`),s("span",{class:"hljs-keyword"},"import"),a(" dark "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui/es/themes/dark'"),a(`
-`),s("span",{class:"hljs-keyword"},"import"),a(" { StyleProvider } "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui'"),a(`
+import{_ as t,p as c,f as o,h as r,M as s,q as n,S as e,P as a}from"./vue-router.esm-bundler.e71c168d.js";const p={components:{}},h={class:"varlet-site-doc"},i=s("h1",null,"\u6697\u9ED1\u6A21\u5F0F",-1),d=s("div",{class:"card"},[s("h3",null,"\u4ECB\u7ECD"),s("p",null,"\u7EC4\u4EF6\u5E93\u7B2C\u4E00\u65B9\u63D0\u4F9B\u4E86\u6697\u9ED1\u6A21\u5F0F\u7684\u4E3B\u9898\uFF0C\u6697\u9ED1\u6A21\u5F0F\u7684\u4F18\u52BF\u5728\u4E8E\u5728\u5F31\u5149\u73AF\u5883\u4E0B\u5177\u6709\u66F4\u9AD8\u7684\u53EF\u8BFB\u6027\u3002")],-1),u={class:"card"},j=s("h3",null,"\u5207\u6362\u4E3B\u9898",-1),_=s("pre",{class:"hljs"},[s("code",null,[s("span",{class:"hljs-tag"},[a("<"),s("span",{class:"hljs-name"},"script"),a(),s("span",{class:"hljs-attr"},"setup"),a(">")]),s("span",{class:"javascript"},[a(`
+`),s("span",{class:"hljs-keyword"},"import"),a(" { StyleProvider, Themes } "),s("span",{class:"hljs-keyword"},"from"),a(),s("span",{class:"hljs-string"},"'@varlet/ui'"),a(`
 
 `),s("span",{class:"hljs-keyword"},"let"),a(" currentTheme = "),s("span",{class:"hljs-literal"},"null"),a(`
 
 `),s("span",{class:"hljs-keyword"},"const"),a(" toggleTheme = "),s("span",{class:"hljs-function"},"() =>"),a(` {
-  currentTheme = currentTheme ? `),s("span",{class:"hljs-literal"},"null"),a(` : dark
+  currentTheme = currentTheme ? `),s("span",{class:"hljs-literal"},"null"),a(` : Themes.dark
   StyleProvider(currentTheme)
 }
 `)]),s("span",{class:"hljs-tag"},[a("</"),s("span",{class:"hljs-name"},"script"),a(">")]),a(`
@@ -18,4 +17,4 @@ import{_ as t,p as c,f as o,h as r,M as s,q as n,S as e,P as a}from"./vue-router
   `),s("span",{class:"hljs-attribute"},"color"),a(": "),s("span",{class:"hljs-built_in"},"var"),a(`(--color-text);
   `),s("span",{class:"hljs-attribute"},"background-color"),a(": "),s("span",{class:"hljs-built_in"},"var"),a(`(--color-body);
 }
-`)])],-1);function v(k,b,f,y,x,w){const l=c("var-site-code-example");return o(),r("div",h,[d,i,s("div",j,[u,n(l,{"playground-ignore":""},{default:e(()=>[_]),_:1}),m,n(l,{"playground-ignore":""},{default:e(()=>[g]),_:1})])])}const N=t(p,[["render",v]]);export{N as default};
+`)])],-1);function v(b,f,k,y,T,x){const l=c("var-site-code-example");return o(),r("div",h,[i,d,s("div",u,[j,n(l,{"playground-ignore":""},{default:e(()=>[_]),_:1}),m,n(l,{"playground-ignore":""},{default:e(()=>[g]),_:1})])])}const N=t(p,[["render",v]]);export{N as default};
