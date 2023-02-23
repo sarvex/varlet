2ebd49f6d8ba343f2cc793064401d3b6d88c1e59
haoziqaq <357229046@qq.com>
2023-02-23 10:58:21 +0000

    Deploying to gh-page from @ varletjs/varlet@bae0245ab57d32a76b3dcebbaa0499ee9d4f42f6 🚀

--- assets/index-8bd3f020.js
+++
@@ -1,1 +0,0 @@
-import{V as n}from"./Paper-0b1de09b.js";n.install=function(a){a.component(n.name,n)};
---
+++ assets/index-9cf88b83.js
@@ -0,0 +1,1 @@
+import{V as n}from"./Paper-80a9f08b.js";n.install=function(a){a.component(n.name,n)};
